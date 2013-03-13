b568a6135560289e508446c31dabc9de6ae4944c
ts-tiefeng.sun <ts-tiefeng.sun@mail.rakuten.com>
2013-03-13 11:26:55 +0800

    add new class

---
+++ src/main/java/com/stf/test/jenkins/TestClass.java
@@ -0,0 +1,28 @@
+/**
+Copyright (c) Rakuten Travel, Inc. All Rights Reserved.
+*
+This program is the information assets which are handled
+as "Strictly Confidential".
+Permission of Use is only admitted in Rakuten Inc.
+Development Department.
+If you don't have permission , MUST not be published,
+broadcast, rewritten for broadcast or publication
+or redistributed directly or indirectly in any medium.
+*
+
+Created on 2013-3-13
+Updated on 2013-3-13
+*/
+package com.stf.test.jenkins;
+
+/**
+ * @author ts-tiefeng.sun
+ *
+ */
+public class TestClass {
+
+    public void printSomething() {
+        TestClass test = new TestClass();
+        test.printSomething();
+    }
+}
